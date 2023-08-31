7ad42767d43971b54dd20d4dd1a1565b7c250a6c
Selva2064775 <Selva2064775>
2023-08-31 15:07:13 +0000

    changes done

--- DEMO_SK/(Clone) Demo Notebook.py
+++ DEMO_SK/(Clone) Demo Notebook.py
@@ -1,2 +1,8 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC Links/Embedded HTML: <a href="https://docs.databricks.com/en/delta/history.html#:~:text=To%20get%20the%20version%20number,lastCommitVersionInSession" target="_blank">Databricks Versioning</a>
 
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Link: <a href="https://docs.databricks.com/en/delta/history.html#:~:text=To%20get%20the%20version%20number,lastCommitVersionInSession" target="_blank">Versioning</a>
